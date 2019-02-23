2614076d0b4d3543828da0bcbced1353e45717d6
Matt Shaner <37387404+mdshaner@users.noreply.github.com>
2019-02-23 14:09:14 -0600

    Update Reproduction Case to include styling file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
     selector: "Home",
     moduleId: module.id,
+    styleUrls: ["./home.component.scss"],
     templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
